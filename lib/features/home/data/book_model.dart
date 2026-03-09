8f670776eb5bc3f0f097a9562053f80c9f8e7e55
Mohamed <mido.basiouny123@gmail.com>
2026-03-09 21:50:41 +0200

    creating book model

---
+++ lib/features/home/data/book_model.dart
@@ -0,0 +1,13 @@
+class BookModel {
+  final String title, auther;
+  final double price, rate;
+  final int numberOfrating;
+
+  BookModel({
+    required this.title,
+    required this.auther,
+    required this.price,
+    required this.rate,
+    required this.numberOfrating,
+  });
+}
