91b73a7d8ff2aaf6a0498260ccfeed7b8684126f
Mohamed <mido.basiouny123@gmail.com>
2026-03-14 03:33:09 +0200

    create HomeRepo

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,6 @@
+import 'package:test1/features/home/data/book_model/book_model.dart';
+
+abstract class HomeRepo {
+  Future<List<BookModel>> fetchFeaturedBooks();
+  Future<List<BookModel>> fetchBestSellerBooksBooks();
+}
