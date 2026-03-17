bcb59c4bce9fd494663223a9b387c1073520c2f8
Mohamed <mido.basiouny123@gmail.com>
2026-03-17 19:30:13 +0200

    providing SimilarBooksCubit

--- lib/core/utils/app_routes.dart
+++ lib/core/utils/app_routes.dart
@@ -1,5 +1,10 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:test1/core/utils/service_locator.dart';
 import 'package:test1/features/home/data/book_model/book_model.dart';
+import 'package:test1/features/home/data/repos/home_repo.dart';
+import 'package:test1/features/home/data/repos/home_repo_imp.dart';
+import 'package:test1/features/home/presentation/manager/display_book_details_cubit/display_book_details_cubit.dart';
 import 'package:test1/features/home/presentation/views/book_details_view.dart';
 import 'package:test1/features/home/presentation/views/home_view.dart';
 import 'package:test1/features/search/presentation/views/search_view.dart';
@@ -17,7 +22,11 @@ sealed class AppRoutes {
       GoRoute(
         path: kBookDetailsView,
         builder: (context, state) {
-          return BookDetailView(bookModel: state.extra as BookModel);
+          return BlocProvider(
+            create: (context) =>
+                SimilarBooksCubit(homeRepo: getIt.get<HomeRepoImp>()),
+            child: BookDetailView(bookModel: state.extra as BookModel),
+          );
         },
       ),
       GoRoute(path: kSearchViewView, builder: (context, state) => SearchView()),
